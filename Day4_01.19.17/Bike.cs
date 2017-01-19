aa4b4d6f3d1d82fd02d1e56c670ffe54f828578f
ksindorf000 <ksindorf000@gmail.com>
2017-01-19 10:25:00 -0500

    Added Day4: More testing

---
+++ Day4_01.19.17/Bike.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_01._19._17
+{
+    public class Bike
+    {
+        private int speed = 0;
+        private double speedModifier = 1;
+        public int speedLimit = 12;
+        public int location = 0;
+
+        public Bike()
+        {
+        }
+
+        public int GetSpeed()
+        {
+            return speed;
+        }
+
+        public double GetModifier()
+        {
+            return speedModifier;
+        }
+
+        public void Accelerate()
+        {
+            if (speed < speedLimit)
+            {
+                speed++;
+            }
+        }
+
+        public void MoveForward()
+        {
+            location += speed; //"In-Place Addition" (10:14AM)
+        }
+
+    }
+}
---
+++ Day4_BikeTest/BikeTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day4_01._19._17;
+
+namespace Day4_BikeTest
+{
+    [TestClass]
+    public class BikeTest
+    {
+        [TestMethod]
+        public void ValuesInitializedCorrectly()
+        {
+            var sut = new Bike();
+            Assert.AreEqual(0, sut.GetSpeed());
+            Assert.AreEqual(1, sut.GetModifier());
+            Assert.AreEqual(0, sut.location);
+        }
+
+        [TestMethod]
+        public void BikeCanAccelerate()
+        {
+            var sut = new Bike();
+            sut.Accelerate();
+            Assert.AreEqual(1, sut.GetSpeed());
+        }
+
+        [TestMethod]
+        public void BikeCantBreakSpeedLimit()
+        {
+            var sut = new Bike();
+            for (int i = 0; i < 15; i ++)
+            {
+                sut.Accelerate();
+            }
+            //Brittle test (9:58AM) breaks easily
+            //Assert.AreEqual(sut.speedLimit, sut.GetSpeed()); 
+            //Better way...
+            Assert.AreEqual(sut.speedLimit, sut.GetSpeed()); 
+        }
+
+        [TestMethod]
+        public void BikeMovesForward()
+        {
+            var sut = new Bike();
+            sut.Accelerate();
+            sut.Accelerate();
+            sut.MoveForward();
+            Assert.AreEqual(2, sut.location);
+        }
+
+        [TestMethod]
+        public void BikeLocationFollowsSpeedLimit()
+        {
+            var sut = new Bike();
+            for (int i = 0; i < 20; i++)
+            {
+                sut.Accelerate();
+            }
+            sut.MoveForward();
+            Assert.AreEqual(sut.speedLimit, sut.location);
+        }
+    }
+}
